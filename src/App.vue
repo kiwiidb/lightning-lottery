b3a3670dcf938081e3b824eedfd8115ab0bb7bb6
Kwinten De Backer <kwinten.de.backer@be-mobile.com>
2019-02-11 23:18:02 +0100

    some work

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Participate />
+    <Participants />
+    <Winners />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Participate from './components/Participate.vue'
+import Participants from './components/Participants.vue'
+import Winners from './components/Winners.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Participate,
+    Participants,
+    Winners
   }
 }
 </script>
